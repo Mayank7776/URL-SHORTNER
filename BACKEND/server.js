076ac58e7b595b388894ed069bbeb425d7a59b25
Your Name <you@example.com>
2025-05-17 13:53:52 +0530

    update cors

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -16,7 +16,7 @@ dotenv.config("./.env")
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173', // React app
+    origin: 'https://url-shortner-henna-five.vercel.app', // vercel React app
     credentials: true 
 }));
 
